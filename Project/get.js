d75fe3aeb6d5bb6fa54e628f1bd9df05b05e9af3
Andres Felipe Herrera Jerez <33936503+Andresxv@users.noreply.github.com>
2019-05-28 16:13:58 -0500

    {+}

--- Project/get.js
+++ Project/get.js
@@ -1,2 +1,7 @@
 var nameStudent = document.getElementsByClassName("dddefault")[0].innerHTML;
 nameStudent;
+var dddefault=document.getElementsByClassName("dddefault");
+var ia="";
+dddefault.array.forEach(function(data) {
+    console.log(data);
+});
