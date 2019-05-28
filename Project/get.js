e0af73eb73f5dc8f6d03ada6a5e7bba75f90ecc5
? <alberto.garcia.2016@upb.edu.co>
2019-05-28 17:17:11 -0500

    Update get.js

--- Project/get.js
+++ Project/get.js
@@ -1,3 +1,11 @@
-var nameStudent = document.getElementsByClassName("dddefault")[0].innerHTML;
-window.alert(nameStudent+"\nMatriculado en",document.getElementsByClassName("ddlabel")[12].innerHTML);
-var nameStudent = document.getElementsByClassName("dddefault")[0].innerHTML;
+var nameStudent = document.getElementsByClassName("dddefault")[0].innerText;
+var values=nameStudent+"\nMatriculado en "+document.getElementsByClassName("ddlabel")[12].innerText+"\n";
+var collection=document.getElementsByClassName("dddefault");
+var aux=0;
+Array.prototype.forEach.call(collection, function(element) {
+    if(element.innerText=="UG"){ //element.colSpan == 5
+        values+=collection[aux-2].innerText+" "+collection[aux-1].innerText+"\n"+collection[aux+1].innerText+" "+collection[aux+2].innerText+" "+parseInt(collection[aux+3].innerText)+"\n";
+    }
+    aux++;
+});
+window.alert(values);
