67fca44e7f1d1164ee773038a82c9e027ed4dcf5
? <alberto.garcia.2016@upb.edu.co>
2019-05-30 11:30:22 -0500

    Update get.js

--- Project/get.js
+++ Project/get.js
@@ -1,18 +1,41 @@
+function createTable(tableData) {
+    var table = document.createElement('table');
+    var tableBody = document.createElement('tbody');
+    tableData.forEach(function(rowData) {
+    var row = document.createElement('tr');
+    rowData.forEach(function(cellData) {
+        var cell = document.createElement('td');
+        cell.appendChild(document.createTextNode(cellData));
+        row.appendChild(cell);
+    });
+    tableBody.appendChild(row);
+    });  
+    table.appendChild(tableBody);
+    document.body.appendChild(table);
+}
+
 var nameStudent = document.getElementsByClassName("dddefault")[0].innerText;
-var values=nameStudent+"\nMatriculado en "+document.getElementsByClassName("ddlabel")[12].innerText+"\n";
+var values=nameStudent+"Matriculado en "+document.getElementsByClassName("ddlabel")[12].innerText;
 var collection=document.getElementsByClassName("dddefault");
 var aux=0;
+var subjects=[];
+var arr=[];
 Array.prototype.forEach.call(collection, function(element) {
     if(element.innerText=="UG"){ //element.colSpan == 5
+
         if(parseInt(collection[aux+2].innerText)>=3){            
             //green
+            values=collection[aux-2].innerText+"-"+collection[aux-1].innerText+"\n"+collection[aux+1].innerText+"\n"+collection[aux+2].innerText+"\n"+parseInt(collection[aux+3].innerText);
+            arr.push(values.split("\n"));
+            //La línea de arriba no va 
         }else if(parseInt(collection[aux+2].innerText)<3){
             //red
         }else{
-            //gray
+            //gray 
         }
-        values+=collection[aux-2].innerText+" "+collection[aux-1].innerText+"\n"+collection[aux+1].innerText+" "+collection[aux+2].innerText+" "+parseInt(collection[aux+3].innerText)+"\n";
+        //values+=collection[aux-2].innerText+" "+collection[aux-1].innerText+"\n"+collection[aux+1].innerText+" "+collection[aux+2].innerText+" "+parseInt(collection[aux+3].innerText)+"\n";
     }
     aux++;
 });
-window.alert(values);
+window.alert(arr);
+createTable(arr);
