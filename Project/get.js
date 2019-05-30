992cb16601fddefa383ef84d69770981042136cf
? <alberto.garcia.2016@upb.edu.co>
2019-05-30 15:28:54 -0500

    Creating csv file download

--- Project/get.js
+++ Project/get.js
@@ -1,28 +1,10 @@
-function createTable(tableData) {
-    var table = document.createElement('table');
-    var tableBody = document.createElement('tbody');
-    tableData.forEach(function(rowData) {
-    var row = document.createElement('tr');
-    rowData.forEach(function(cellData) {
-        var cell = document.createElement('td');
-        cell.appendChild(document.createTextNode(cellData));
-        row.appendChild(cell);
-    });
-    tableBody.appendChild(row);
-    });  
-    table.appendChild(tableBody);
-    document.body.appendChild(table);
-}
-
-var nameStudent = document.getElementsByClassName("dddefault")[0].innerText;
-var values=nameStudent+"Matriculado en "+document.getElementsByClassName("ddlabel")[12].innerText;
+var student=document.getElementsByClassName("dddefault")[0].innerText+"\nMatriculado en "+document.getElementsByClassName("ddlabel")[12].innerText+"\n\n\nPorfavor descargue el archivo y cárguelo en la extensión de su derecha.";
 var collection=document.getElementsByClassName("dddefault");
 var aux=0;
 var subjects=[];
-var arr=[];
+var arr=[]; 
 Array.prototype.forEach.call(collection, function(element) {
     if(element.innerText=="UG"){ //element.colSpan == 5
-
         if(parseInt(collection[aux+2].innerText)>=3 && !isNaN(collection[aux+3].innerText)){            
             //green
             values=collection[aux-2].innerText+"-"+collection[aux-1].innerText+"\n"+collection[aux+1].innerText+"\n"+collection[aux+2].innerText+"\n"+parseInt(collection[aux+3].innerText);
@@ -37,5 +19,19 @@ Array.prototype.forEach.call(collection, function(element) {
     }
     aux++;
 });
-window.alert(arr);
-createTable(arr);
+window.alert(student);//falta el promedio y el resto de cosas
+
+let csvContent = "data:text/csv;charset=utf-8," 
+    + arr.map(e => e.join(",")).join("\n");
+//var encodedUri = encodeURI(csvContent);
+//window.open(encodedUri);
+var encodedUri = encodeURI(csvContent);
+var link = document.createElement("a");
+link.setAttribute("href", encodedUri);
+link.setAttribute("download", "my_data.csv");
+document.body.appendChild(link); // Required for FF
+
+link.click(); // This will download the data file named "my_data.csv".
+
+//var blob = new Blob(["This is my first text."], {type: "text/plain;charset=utf-8"});
+//saveAs(blob, "testfile21.txt");
